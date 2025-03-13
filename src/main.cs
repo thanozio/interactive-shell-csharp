c05369ee0b5c1e0b48dad1e65f33510e07a75d33
Thanozio <thanozio@proton.me>
2025-03-13 13:55:14 +0200

    refactor: reject every input

--- src/main.cs
+++ src/main.cs
@@ -1,12 +1,4 @@
-using System.Net;
-using System.Net.Sockets;
-
-// Uncomment this line to pass the first stage
 Console.Write("$ ");
 
-// Wait for user input
 string? command = Console.ReadLine();
-if (command is not null)
-{
-    Console.WriteLine($"{command}: command not found");
-}
+Console.WriteLine($"{command}: command not found");
